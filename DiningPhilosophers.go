482691e6652ce5ef10f83350a3e8c3adfafee7ed
habr <habr@itu.dk>
2023-09-06 12:13:49 +0200

    fork inital requesting developed

--- DiningPhilosophers.go
+++ DiningPhilosophers.go
@@ -1,39 +1,51 @@
 package main
 
-import "fmt"
+var philChans [5]chan bool
 
-var leftChans [5]chan bool
-var rightChans [5]chan bool
-
-func status(string status){
-	fmt.Println(status)
-}
+var forkChans [5]chan bool
 
 func philosopher(i int) {
-	var leftChan = leftChans[i];
-	var rightChan = rightChans[i];
-	
-	if ()
+	rFork := requestFork(i)
+	lFork := requestFork((i - 1) % 5)
 
+	if rFork != lFork {
+		if lFork {
+			rFork = requestFork(i)
+		} else {
+			lFork = requestFork((i - 1) % 5)
+		}
+	}
+}
+
+func requestFork(i int) bool {
+	forkChans[i] <- true
+	return <-forkChans[i]
 }
 
 func fork(i int) {
+	var inUse = false
 
-}
-func create(){
-	// Creates 5 philosophers, 5 forks and channels for the right and the left fork
-	for i := 0; i < 5; i++ {
-		leftChans[i] = make(chan bool)
-		rightChans[i] = make(chan bool)
-		go philosopher(i)
-		go fork(i)
+	for {
+		<-forkChans[i] //waits for signal
+		if inUse {
+			forkChans[i] <- false
+		} else {
+			inUse = true
+			forkChans[i] <- true
+		}
 	}
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-	create()
+	// Creates 5 philosophers, 5 forks and channels for the right and the left fork
+	for i := 0; i < 5; i++ {
+		philChans[i] = make(chan bool)
+		forkChans[i] = make(chan bool)
+		go fork(i)
+		go philosopher(i)
+	}
 
-	
+	for {
 
+	}
 }
