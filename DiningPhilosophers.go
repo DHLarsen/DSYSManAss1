2a9deb4c90a1e766c6c35f27d4e65945e00c6f02
habr <halfdanemb@gmail.com>
2023-09-06 15:06:55 +0200

    initial eat then deadlock

--- DiningPhilosophers.go
+++ DiningPhilosophers.go
@@ -1,18 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 var philChans [5]chan bool
 
 var forkChans [5]chan bool
 
 func philosopher(i int) {
-	rFork := requestFork(i)
-	lFork := requestFork((i - 1) % 5)
 
-	if rFork != lFork {
-		if lFork {
-			rFork = requestFork(i)
+	for {
+		rFork := requestFork((i + 1) % 5)
+		lFork := requestFork(i)
+
+		if rFork != lFork {
+			delay := rand.Intn(10)
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+			rFork = requestFork((i + 1) % 5)
+			lFork = requestFork(i)
+		}
+		if rFork && lFork {
+			fmt.Println("Philosopher ", i, " eating")
+			time.Sleep(1 * time.Second)
 		} else {
-			lFork = requestFork((i - 1) % 5)
+			if rFork {
+				releaseFork((i + 1) % 5)
+			}
+			if lFork {
+				releaseFork(i)
+			}
+			time.Sleep(4 * time.Millisecond)
 		}
 	}
 }
@@ -22,16 +42,23 @@ func requestFork(i int) bool {
 	return <-forkChans[i]
 }
 
+func releaseFork(i int) {
+	forkChans[i] <- false
+}
+
 func fork(i int) {
 	var inUse = false
 
 	for {
-		<-forkChans[i] //waits for signal
-		if inUse {
-			forkChans[i] <- false
-		} else {
-			inUse = true
-			forkChans[i] <- true
+		if <-forkChans[i] { //requesting forks
+			if inUse {
+				forkChans[i] <- false
+			} else {
+				inUse = true
+				forkChans[i] <- true
+			}
+		} else { //releasing forks
+			inUse = false
 		}
 	}
 }
