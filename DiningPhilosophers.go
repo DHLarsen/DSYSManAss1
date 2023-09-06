11727845294aa69cc73c5c10d2c0b8c1d157b7c7
Daniel Holm Larsen <daniel.holm.larsen@gmail.com>
2023-09-06 11:29:51 +0200

    Framework

--- DiningPhilosophers.go
+++ DiningPhilosophers.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func philosopher(i int) {
+
+}
+
+func fork(i int) {
+
+}
+
 func main() {
 	fmt.Println("Hello, World!")
+
+	var chans [5]chan bool
+	for i := 0; i < 5; i++ {
+		go philosopher(i)
+		go fork(i)
+		chans[i] = make(chan bool)
+	}
+
 }
